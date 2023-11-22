410a824173ea9646d7d95b67877c9064ba2b1832
so-dami <so94dami@gmail.com>
2023-11-22 10:19:17 +0900

    ㅇ

--- ohora/03.구현소스/js/main.js
+++ ohora/03.구현소스/js/main.js
@@ -1,30 +1,28 @@
 // main.js
 
-console.log("main js 호출");
-
 // 메인 배경 효과
 // 대상: .lbg, .rbg
-const lbg = document.querySelector(".lbg");
-const rbg = document.querySelector(".rbg");
+const lbg = document.querySelector('.lbg');
+const rbg = document.querySelector('.rbg');
 
 // 왼쪽
-lbg.addEventListener("mouseenter", function () {
-    this.classList.add("on");
-    rbg.classList.remove("on");
+lbg.addEventListener('mouseenter', function () {
+  this.classList.add('on');
+  rbg.classList.remove('on');
 });
 
 // 오른쪽
-rbg.addEventListener("mouseenter", function () {
-    this.classList.add("on");
-    lbg.classList.remove("on");
+rbg.addEventListener('mouseenter', function () {
+  this.classList.add('on');
+  lbg.classList.remove('on');
 });
 
 // 햄버거 버튼
 // 대상: .ham, .mlist
-const ham = document.querySelector(".ham");
-const mlist = document.querySelector(".mlist");
+const ham = document.querySelector('.ham');
+const mlist = document.querySelector('.mlist');
 
-ham.addEventListener("click", function () {
-    this.classList.toggle("on");
-    mlist.classList.toggle("on");
+ham.addEventListener('click', function () {
+  this.classList.toggle('on');
+  mlist.classList.toggle('on');
 });
--- ohora/03.구현소스/js/sub_ham.js
+++ ohora/03.구현소스/js/sub_ham.js
@@ -1,9 +1,9 @@
 // 햄버거 버튼
 // 대상: .ham, .mlist
-const ham = document.querySelector(".ham");
-const mlist = document.querySelector(".mlist");
+const ham = document.querySelector('.ham');
+const mlist = document.querySelector('.mlist');
 
-ham.addEventListener("click", function () {
-    this.classList.toggle("on");
-    mlist.classList.toggle("on");
+ham.addEventListener('click', function () {
+  this.classList.toggle('on');
+  mlist.classList.toggle('on');
 });
