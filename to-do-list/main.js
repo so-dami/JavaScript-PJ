569c80c2f972e79982cf2f57590f3df62bd08254
so-dami <so94dami@gmail.com>
2023-12-22 03:35:10 +0900

    dd

--- to-do-list/main.js
+++ to-do-list/main.js
@@ -1,133 +1,243 @@
-console.log('to do list page');
-
-// HTML 생성1
-/*
-  var a = document.createElement('p');
-  a.innerHTML = '안녕';
-  a.classList.add('test-p');
-  document.querySelector('#test').appendChild(a);
-*/
-
-// HTML 생성2
-// insertAdjacentHTML - 문자형 HTML을 추가해주는 함수
-// 추가 말고 다 갈고 싶으면 innerHTML
-// insertAdjacentHTML('넣을 위치', '넣을 HTML');
-// beforeend - 안쪽 맨 끝
-/*
-  var temp = `<p>Hello</p>`;
-  document.querySelector('#test').insertAdjacentHTML('beforeend', temp);
-*/
-
-// jQuery ver.
-// $('#test').append(temp);
-
-// localStorage
-/*
-  - key:value 형식으로 저장
-  - 문자 데이터만 저장 가능함
-  - 사이트에 재접속을 해도 값이 유지됨
-
-  [ 사용법 ]
-
-  1. 저장
-  - localStorage.setItem('key', 'value');
-  
-  2. 출력
-  - localStorage.getItem('key');
-
-  3. 삭제
-  - localStorage.removeItem('key');
-
-  
-  [ object, array 저장법 ]
-
-  1. array, object -> JSON으로 변환
-  - JSON.stringify(자료);
-
-  3. JSON -> array, object로 변환
-  - JSON.parse(자료);
-
-  
-  [ 수정법 ]
-
-  1. getItem -> 수정 -> setItem
-*/
-
-let i = 0;
+// 오늘의 할 일에 새로 입력된 값을 넣을 변수
 let newTodo = '';
 
+// 오늘의 할 일 input 값
 let todoTxt = document.querySelector('#input-txt');
+
+// + 버튼
 let plusBtn = document.querySelector('.plus-btn');
+
 let ul = document.querySelector('.list-wrap > ul');
 
+// 공백 체크 모달창
 let blankChk = document.querySelector('#blank-chk');
-let delChk = document.querySelector('#del-chk');
-let closeBtn = document.querySelector('#close-btn');
-let yesBtn = document.querySelector('#yes-btn');
-let noBtn = document.querySelector('#no-btn');
 
-// plus button - 할 일 추가
+// 공백 체크 모달창 닫기 버튼
+let closeBtn = document.querySelector('#close-btn');
+
+// liList
+let liList;
+
+// count
+let i;
+if (localStorage.getItem('idx') == null) {
+  i = 0;
+  localStorage.setItem('idx', i);
+} else {
+  i = localStorage.getItem('idx');
+}
+
+// 새로고침 후에도 데이터, checked 유지 시키기
+if (localStorage.getItem('todo') !== null) {
+  let todoGet = JSON.parse(localStorage.getItem('todo'));
+
+  for (let i = 0; i < todoGet.length; i++) {
+    let hcode = `
+      <li class="list-item flex">
+        <div class="chk-box">
+          <input type="checkbox" name="check${todoGet[i]['id']}" id="check${todoGet[i]['id']}" value="${
+      todoGet[i]['txt']
+    }" ${todoGet[i]['checked'] == true ? 'checked' : ''} onchange="chgChecked(this)" />
+          <label class="chk-label" for="check${todoGet[i]['id']}">${todoGet[i]['txt']}</label>
+        </div>
+        <div class="del-box">
+          <button class="del-btn" type="button" onClick="listDelete(this)">
+            <div></div>
+          </button>
+        </div>
+      </li>
+    `;
+    ul.innerHTML += hcode;
+  } // for //
+  liList = document.querySelectorAll('input[type="checkbox"]');
+} // if //
+
+// plus button 클릭 시 - li 추가
 plusBtn.addEventListener('click', function () {
-  // 공백 체크 - input 공백일 떄
+  // 공백 체크 - input 값이 공백일 때
   if (todoTxt.value == '') {
     blankChk.classList.add('show-modal');
+  } // if //
 
-    // 닫기 버튼 누를 때 - 모달창 닫기
-    closeBtn.addEventListener('click', function () {
-      blankChk.classList.remove('show-modal');
-    });
-
-    // 검정 배경 누를 때 - 모달창 닫기
-    blankChk.addEventListener('click', function () {
-      blankChk.classList.remove('show-modal');
-    });
-  }
-
-  // 공백 체크 - input 공백 아닐 때
+  // 공백 체크 - input 값이 공백이 아닐 때
   else {
-    i++;
-    newTodo = todoTxt.value;
+    let todoLocal;
+    let localGet = JSON.parse(localStorage.getItem('todo'));
 
+    i++;
+    localStorage.setItem('idx', i);
+
+    // 오늘의 할 일 input 값 변수에 넣기
+    newTodo = { txt: todoTxt.value, checked: false, id: i };
+
+    // localStorage 추가
+    if (localStorage.getItem('todo') == null) {
+      todoLocal = [];
+      todoLocal.push(newTodo);
+      localStorage.setItem('todo', JSON.stringify(todoLocal));
+    } else {
+      localGet.push(newTodo);
+      localStorage.setItem('todo', JSON.stringify(localGet));
+    } // else //
+
+    // li 추가
     let li = `
-    <li class="list-item flex">
-      <div class="chk-box">
-        <input type="checkbox" name="chk${i}" id="chk${i}" value="${newTodo}" />
-        <label class="chk-label" for="chk${i}">${newTodo}</label>
-      </div>
-      <div class="del-box">
-        <button class="del-btn" type="button">
-          <div></div>
-        </button>
-      </div>
-    </li>
-  `;
+      <li class="list-item flex">
+        <div class="chk-box">
+          <input type="checkbox" name="check${i}" id="check${i}" value="${newTodo['txt']}" onchange="chgChecked(this)" />
+          <label class="chk-label" for="check${i}">${newTodo['txt']}</label>
+        </div>
+        <div class="del-box">
+          <button class="del-btn" type="button" onClick="listDelete(this)">
+            <div></div>
+          </button>
+        </div>
+      </li>
+    `;
 
     ul.innerHTML += li;
     todoTxt.value = '';
+  } // else //
+  liList = document.querySelectorAll('input[type="checkbox"]');
+  console.log(liList);
+}); // plus-btn click //
 
-    let delBtn = document.querySelectorAll('.del-btn');
-    li = document.querySelectorAll('.list-item');
+function chgChecked(e) {
+  console.log('change', e.id, e.checked);
 
-    // delete button
-    // 삭제 여부 물어보기
-    delBtn.forEach((a, i) => {
-      a.addEventListener('click', function () {
-        delChk.classList.add('show-modal');
+  // e의 checked 값
+  let isChecked = e.checked;
 
-        // 아니오 - 모달창 닫기
-        noBtn.addEventListener('click', function () {
-          delChk.classList.remove('show-modal');
-        });
+  // localStorage에서 데이터 가져오기
+  let getItem = JSON.parse(localStorage.getItem('todo'));
 
-        // 네 - 할 일 삭제
-        yesBtn.addEventListener('click', function (e) {
-          delChk.classList.remove('show-modal');
+  // e.id에서 문자 지우기
+  let splitCheck = e.id.split('check')[1];
+  console.log(splitCheck);
 
-          setTimeout(() => {
-            li[i].remove();
-          }, 300);
-        });
-      });
-    });
-  }
+  // e의 checked가 true면?
+  if (isChecked) {
+    // splitCheck와 'todo'의 id와 일치하는 데이터 찾아서 checked 값 변경하기
+    getItem.forEach((a) => {
+      if (a['id'] == splitCheck) {
+        a['checked'] = true;
+        localStorage.setItem('todo', JSON.stringify(getItem));
+      }
+    }); // forEach //
+  } else {
+    getItem.forEach((a) => {
+      if (a['id'] == splitCheck) {
+        a['checked'] = false;
+        localStorage.setItem('todo', JSON.stringify(getItem));
+      }
+    }); // forEach //
+  } // else //
+} // chgChecked //
+
+// 닫기 버튼 클릭 시 - 모달창 닫기
+closeBtn.addEventListener('click', function () {
+  blankChk.classList.remove('show-modal');
 });
+
+// 검정 배경 클릭 시 - 모달창 닫기
+blankChk.addEventListener('click', function () {
+  blankChk.classList.remove('show-modal');
+});
+
+// 삭제
+// function listDelete() {
+//   // '-' 버튼
+//   let delBtn = document.querySelectorAll('.del-btn');
+
+//   // 삭제 여부 묻는 모달창
+//   let delChk = document.querySelector('#del-chk');
+
+//   // '예' 버튼
+//   let yesBtn = document.querySelector('#yes-btn');
+
+//   // 삭제할 li
+//   let delList = document.querySelectorAll('.list-item');
+
+//   // delete button 클릭 시
+//   delBtn.forEach((a, i) => {
+//     a.addEventListener('click', function () {
+//       // let delList = a.parentElement.parentElement;
+
+//       // 삭제 여부 물어보기
+//       delChk.classList.add('show-modal');
+
+//       // 예 클릭 시
+//       yesBtn.addEventListener('click', function () {
+//         // 모달창 닫기
+//         delChk.classList.remove('show-modal');
+
+//         // localStorage에서 해당 데이터 삭제
+//         let local = JSON.parse(localStorage.getItem('todo'));
+
+//         local.splice(i, 1);
+//         localStorage.setItem('todo', JSON.stringify(local));
+
+//         console.log(delList[i]);
+
+//         // 해당 li 삭제
+//         delList[i].remove();
+//       });
+//     });
+//   });
+// }
+
+// 삭제
+function listDelete(e) {
+  let deleteBtn = e;
+
+  // 삭제 여부 묻는 모달창
+  let delChk = document.querySelector('#del-chk');
+
+  // 삭제 여부 모달창 열기
+  delChk.classList.add('show-modal');
+
+  // '예' 버튼
+  let yesBtn = document.querySelector('#yes-btn');
+
+  // '아니오' 버튼
+  let noBtn = document.querySelector('#no-btn');
+
+  // '예' 클릭 시
+  yesBtn.addEventListener('click', function () {
+    // 삭제 여부 모달창 닫기
+    delChk.classList.remove('show-modal');
+
+    // 해당 삭제 버튼 상단의 li 찾기
+    let delLi = deleteBtn.parentElement.parentElement;
+
+    // 삭제할 input 찾기
+    let delInput = delLi.querySelector('input[type="checkbox"]');
+
+    // 값: check숫자
+    let id = delInput.id.split('check')[1];
+
+    // li 삭제하기
+    delLi.remove();
+
+    // localStorage 데이터 삭제
+    let delLocal = JSON.parse(localStorage.getItem('todo'));
+
+    let result = delLocal.filter((value) => {
+      return value['id'] != id;
+    });
+
+    localStorage.setItem('todo', JSON.stringify(result));
+  });
+
+  // '아니오' 클릭 시
+  noBtn.addEventListener('click', function () {
+    // 모달창 닫기
+    delChk.classList.remove('show-modal');
+  });
+
+  // 검정 배경 클릭 시
+  delChk.addEventListener('click', function () {
+    // 모달창 닫기
+    delChk.classList.remove('show-modal');
+  });
+}
